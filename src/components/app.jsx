0d6de48ad4b421961fafd015b3f609de951c1cd2
Roldan <roldan.barreto@mercadolibre.cl>
2021-11-28 16:05:21 -0300

    adding webpack-dev-server

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,6 +4,6 @@ import logo from '../../public/assets/logo.svg';
 export default function() {
     return <div className="ui-wrapper">
         <img src={logo} with={200} alt="base logo"/>
-        <h1 className="ui-wrapper-text">React from scratch</h1>
+        <h1 className="ui-wrapper-text">React from scratch!!!!</h1>
     </div>
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,16 @@
+const path = require('path');
 const { merge } = require('webpack-merge')
 const webpackDefaultConfig = require('./webpack.config');
 
 
 const config = {
     mode: 'development',
+    devServer: {
+        port: 3000,
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        }
+    }
 }
 
 /** @type {import('webpack').Configuration} */
