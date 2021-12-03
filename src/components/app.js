34d6fcd3facd9c92e4ce1dc96fdbe5b788ab1ae0
Roldan <roldan.barreto@mercadolibre.cl>
2021-12-03 14:50:29 -0300

    Improving some styles

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import logo from '../../public/assets/logo.svg';
 
 export default function() {
     return <div className="ui-wrapper">
-        <img src={logo} with={200} alt="base logo"/>
+        <img className="ui-wrapper-logo" src={logo} alt="base logo"/>
         <h1 className="ui-wrapper-text">React from scratch!!!!</h1>
     </div>
 }
